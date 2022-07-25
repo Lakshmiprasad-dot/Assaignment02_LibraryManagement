4ccbc3b6c588ec036e65f8f4f6cb571a7a682110
LakshmiprasadPasupuleti <lakshmiprasadpasupuleti9034@gmail.com>
2022-07-25 15:14:09 +0530

    Add project files.

---
+++ cs_con_LibManagementSys/Librarian_Menu.cs
@@ -0,0 +1,52 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace cs_con_LibManagementSys
+//{
+//    internal class Librarian_Menu
+//    {
+//        public string Book_Name;
+//        private int Book_Id;
+//        public static void Librarian__Option()
+//        {
+//            Console.WriteLine("What do you want to manage today ");
+//            Console.WriteLine("For 'Books' Enter [ 1 ]");
+//            Console.WriteLine("For 'NewsPapers' Enter [ 2 ]");
+//            Console.WriteLine("Choose your option Here ");
+//            int Librarian_Option = int.Parse(Console.ReadLine());
+//            if (Librarian_Option == 1)
+//            {
+//                Console.WriteLine("***Welcome to the  'Books' Section");
+//                Console.WriteLine();
+//                Console.WriteLine("Please Select your option ,");
+//                Console.WriteLine("Add Book");
+//                Console.WriteLine("Remove Book");
+//                Console.WriteLine("Show the list of books ");
+//                create_book();
+//            }
+//            else if (Librarian_Option == 2)
+//            {
+//                Console.WriteLine("You had chosen 'Newspapres' Option");
+//            }
+//            else
+//            {
+//                Console.Clear();
+//                Console.WriteLine("You Chosen a Wrong Option, Please Choose Again");
+//                Console.WriteLine();
+//                Librarian__Option();
+//            }
+//            void create_book()
+//            {
+//                Librarian_Menu book1 = new Librarian_Menu();
+//                Console.Write("Enter the book name here :");
+//                book1.Book_Name = (Console.ReadLine());
+//                Console.Write("Enter the book Id here :");
+//                book1.Book_Id = int.Parse(Console.ReadLine());
+//                Console.WriteLine("Book is " + book1.Book_Name + " and book ID is " + book1.Book_Id);
+
+//            }
+//        }
+//    }
+//}
+
---
+++ cs_con_LibManagementSys/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace cs_con_LibManagementSys
+{
+    internal class Program
+    {
+        public static void Startmeathod(int For_Person_Identify)
+        {
+            if (For_Person_Identify == 1)
+            {
+                Console.Clear();
+                Console.WriteLine("You are a Librarian");
+               // Librarian_Menu.Librarian__Option();
+            }
+            else if (For_Person_Identify == 2)
+            {
+                Console.Clear();
+                Console.WriteLine("Borrower");
+            }
+            else
+            {
+                Console.WriteLine("You Chosen a Wrong Option, Please Choose Again");
+                Console.WriteLine();
+                Console.WriteLine("Choose your option Here ");
+                For_Person_Identify = int.Parse(Console.ReadLine());
+                Startmeathod(For_Person_Identify);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Welcome to the Library Management System *********");
+            Console.WriteLine();
+            Console.WriteLine("MENU");
+            Console.WriteLine();
+            Console.WriteLine("Who Are you ? (Librarian / Borrower):");
+            Console.WriteLine("If you are Librarian  : Click 1");
+            Console.WriteLine("If you are Borrower  : Click 2");
+            Console.WriteLine();
+            Console.Write("Choose your option Here :");
+            int value = int.Parse(Console.ReadLine());
+            Startmeathod(value);
+        }
+    }
+}
